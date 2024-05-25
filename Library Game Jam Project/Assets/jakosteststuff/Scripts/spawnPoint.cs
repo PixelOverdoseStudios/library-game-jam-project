2930447b6ba983fd94e6ddc0460e3a51d42c6971
ucnjakob <72885773+jakob12dk@users.noreply.github.com>
2024-05-25 10:16:41 +0200

    updates to the spawning

--- Library Game Jam Project/Assets/jakosteststuff/Scripts/spawnPoint.cs
+++ Library Game Jam Project/Assets/jakosteststuff/Scripts/spawnPoint.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class spawnPoint : MonoBehaviour
 {
     [Header("spawn objects")]
-    [SerializeField] private GameObject prefabToSpawn;
+    [SerializeField] private GameObject[] prefabToSpawn;
     [SerializeField] private int numberOfObjectsToSpawn = 1;
     [SerializeField] private float spawnDelay = 1.0f;
 
@@ -18,12 +18,15 @@ public class spawnPoint : MonoBehaviour
     // Coroutine to spawn prefabs with a delay
     IEnumerator SpawnPrefabsWithDelay()
     {
-        if (prefabToSpawn != null)
+        if (prefabToSpawn != null && prefabToSpawn.Length > 0)
         {
             for (int i = 0; i < numberOfObjectsToSpawn; i++)
             {
-                Instantiate(prefabToSpawn, transform.position, transform.rotation);
-                yield return new WaitForSeconds(spawnDelay); 
+                // Select a random prefab from the array
+                GameObject randomPrefab = prefabToSpawn[Random.Range(0, prefabToSpawn.Length)];
+                // Instantiate the random prefab
+                Instantiate(randomPrefab, transform.position, transform.rotation);
+                yield return new WaitForSeconds(spawnDelay);
             }
         }
         else
