08e84a2a60aa83b7540c98b44823a21090fdeaae
ucnjakob <72885773+jakob12dk@users.noreply.github.com>
2024-05-25 20:06:19 +0200

    added random spawn timers

--- Library Game Jam Project/Assets/jakosteststuff/Scripts/spawnPoint.cs
+++ Library Game Jam Project/Assets/jakosteststuff/Scripts/spawnPoint.cs
@@ -7,8 +7,10 @@ public class spawnPoint : MonoBehaviour
     [Header("spawn objects")]
     [SerializeField] private GameObject[] prefabToSpawn;
     [SerializeField] private int numberOfObjectsToSpawn = 1;
-    [SerializeField] private float spawnDelay = 1.0f;
-
+    [SerializeField] private float spawnDelay;
+    [Header("time between spawns")]
+    [SerializeField] private float minSpawnTimer = 1.0f;
+    [SerializeField] private float maxSpawnTimer = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +20,7 @@ public class spawnPoint : MonoBehaviour
     // Coroutine to spawn prefabs with a delay
     IEnumerator SpawnPrefabsWithDelay()
     {
+        
         if (prefabToSpawn != null && prefabToSpawn.Length > 0)
         {
             for (int i = 0; i < numberOfObjectsToSpawn; i++)
@@ -26,6 +29,7 @@ public class spawnPoint : MonoBehaviour
                 GameObject randomPrefab = prefabToSpawn[Random.Range(0, prefabToSpawn.Length)];
                 // Instantiate the random prefab
                 Instantiate(randomPrefab, transform.position, transform.rotation);
+                spawnDelay = Random.RandomRange(minSpawnTimer, maxSpawnTimer);
                 yield return new WaitForSeconds(spawnDelay);
             }
         }
