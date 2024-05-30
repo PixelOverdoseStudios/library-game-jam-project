42774bea528a90fdccee9125d556677cb426e368
PixelOverdoseStudios <123806965+PixelOverdoseStudios@users.noreply.github.com>
2024-05-30 18:03:04 -0500

    Testing

--- Library Game Jam Project/Assets/Panda's Folder/Scripts/Construction Scripts/BookshelveConstructionLogic.cs
+++ Library Game Jam Project/Assets/Panda's Folder/Scripts/Construction Scripts/BookshelveConstructionLogic.cs
@@ -22,4 +22,74 @@ public class BookshelveConstructionLogic : MonoBehaviour
                 break;
         }
     }
+
+    public void SetSeatingColor(int value)
+    {
+        switch (value)
+        {
+            case 0:
+                DeactivateAllSeats();
+                seatingColors[value].SetActive(true);
+                break;
+            case 1:
+                DeactivateAllSeats();
+                seatingColors[value].SetActive(true);
+                break;
+            case 2:
+                DeactivateAllSeats();
+                seatingColors[value].SetActive(true);
+                break;
+            case 3:
+                DeactivateAllSeats();
+                seatingColors[value].SetActive(true);
+                break;
+            case 4:
+                DeactivateAllSeats();
+                seatingColors[value].SetActive(true);
+                break;
+        }
+    }
+
+    public void SetCouchColor(int value)
+    {
+        switch (value)
+        {
+            case 0:
+                DeactivateAllCouches();
+                couchColors[value].SetActive(true);
+                break;
+            case 1:
+                DeactivateAllCouches();
+                couchColors[value].SetActive(true);
+                break;
+            case 2:
+                DeactivateAllCouches();
+                couchColors[value].SetActive(true);
+                break;
+            case 3:
+                DeactivateAllCouches();
+                couchColors[value].SetActive(true);
+                break;
+            case 4:
+                DeactivateAllCouches();
+                couchColors[value].SetActive(true);
+                break;
+        }
+    }
+
+    private void DeactivateAllSeats()
+    {
+        foreach(GameObject seat in seatingColors)
+        {
+            seat.SetActive(false);
+        }
+    }
+
+    private void DeactivateAllCouches()
+    {
+        foreach (GameObject couch in couchColors)
+        {
+            couch.SetActive(false);
+        }
+    }
 }
