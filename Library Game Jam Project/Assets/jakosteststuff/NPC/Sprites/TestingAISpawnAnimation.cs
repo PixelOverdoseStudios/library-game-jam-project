6b92159bcd7371301e7b541a68949fb58e22223b
PixelOverdoseStudios <123806965+PixelOverdoseStudios@users.noreply.github.com>
2024-06-02 21:34:48 -0500

    Panda 06/02/2024

--- Library Game Jam Project/Assets/jakosteststuff/NPC/Sprites/TestingAISpawnAnimation.cs
+++ Library Game Jam Project/Assets/jakosteststuff/NPC/Sprites/TestingAISpawnAnimation.cs
@@ -7,6 +7,14 @@ public class TestingAISpawnAnimation : MonoBehaviour
     [SerializeField] private GameObject membershipAnimation;
     [SerializeField] private Transform animationSpawnLocation;
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SpawnAnimation();
+        }
+    }
+
     private void SpawnAnimation()
     {
         Instantiate(membershipAnimation, animationSpawnLocation.transform.position, Quaternion.identity);
